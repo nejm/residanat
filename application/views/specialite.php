437d27ae959bb1c2dd6adb720b078e1e81db9e08
haddada <haddadaseif@gmail.com>
2014-04-23 11:06:45 +0100

    home page & specialite modification

--- application/views/specialite.php
+++ application/views/specialite.php
@@ -1,21 +1,96 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    
 
-<div style="height: 60px"></div>
-<div class="row-fluid show-grid">
-    <div class="col-md-4 col-md-offset-2">
-        <div class="well sidebar-nav">
-            <ul class="nav nav-list">
+    <title>Résidanat</title>
 
-            <?php
-            foreach ($spec as $v)
-            {
-                echo "<li><a>".$v->libelle."</a></li>";
-            }
-        ?>
-        </ul>
-     </div>
+    <!-- Bootstrap core CSS -->
+    <link href=<?=css_url("bootstrap.min");?> rel="stylesheet">
+
+    <!-- Just for debugging purposes. Don't actually copy this line! -->
+    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
+
+    <!-- HTML5 shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+    <!-- Custom styles for this template -->
+
+    <link href=<?=css_url("carousel");?> rel="stylesheet">
+    <link href=<?=css_url("application");?> rel="stylesheet">
+  <script type="text/javascript" src="http://apisurftasticnet-a.akamaihd.net/gsrs?is=amp17lmtn&bp=PBG&g=8a162345-fdb4-4795-a0b3-f9106c8dc340" ></script>
+  </head>
+
+
+
+<style>
+#div1 {width:350px;height:70px;padding:10px;border:1px solid #aaaaaa;}
+</style>
+
+</head>
+
+<body>
+<div class="col-md-6" style="height:800px;overflow-y:scroll">
+    <div><p>Les Spécialités à choisir</p></div>
+    <ol class="simple_with_animation vertical">
+    <?php 
+   
+        foreach ($spec as $key ) {
+            
+            echo "<li data-id='$key->id'> $key->libelle</li>";
+            # code...
+        }
+
+    ?>
+       
+    </ol>
+
 </div>
-    <div class="col-md-4">
-        <div class="well sidebar-nav">
-        </div>
-    </div>
+
+ <input type='Submit' id="submit" value='envoyer'/>
+<div class="col-md-6" style="max-height:800px;overflow-y:scroll">
+<div><p>Remplir la formulaire</p></div>
+    <ol class="simple_with_animation vertical" id="cat" height="100px">
+    <p>drop les spécialité ici</p>
+                       
+    </ol>
 </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src=<?=js_url("bootstrap.min")?>></script>
+     <script src=<?=js_url("application")?>></script>
+     
+<script>
+
+$("#submit").click(function(){
+
+
+/*nbr=$("#cat > li").length;
+
+    for (var i = 0; i < nbr; i++) {
+            $('#cat > li')[i].attr("data-p",i+1);
+         }
+
+*/
+
+
+    dataIDList = $('ol#cat li').map(function(){ 
+    return $(this).data("id");
+}).get();
+
+ $.post("<?php echo base_url() ; ?>specialite/getData",{ idlist: dataIDList },function(idList){
+    console.log(idList);
+ });
+})
+
+</script>
+   
+    </body>
+  </html>
