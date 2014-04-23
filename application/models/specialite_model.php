a11600be2260452d71edb13081a588716d147b38
haddada <haddadaseif@gmail.com>
2014-04-23 11:52:16 +0100

    fixe specialite_model

--- application/models/specialite_model.php
+++ application/models/specialite_model.php
@@ -19,24 +19,20 @@ class Specialite_model extends CI_Model{
         return $q->result();
     }
 
-/*
+
     function getSum()
     {
         return $this->db->select_sum('nbr_place')->get('specialite')->row();
 
     }
 
-    function doChoix($cin,$code,$p){
-        $query="insert into choix_condidats (cin,code_specialite,priorite) values(?,?,?)";
-        $res=$this->db->($query,array($cin,$code,$p))
-    }
 
 
     function getNonChoisi($cin){
         $query="select libelle from specialite where code_specialite not in (select code_specialite from choix_candidats where cin =?)";
         $res=$this->db->query($query,array($cin));
         return $res->result();
-    }*/
+    }
     function getById($id){
         $query="select code_specialite from specialite where id = ?";
         $res=$this->db->query($query, array($id));
